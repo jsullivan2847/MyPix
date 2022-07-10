d8402e701ceb653fb41ee08f4b170b466a478926
James Sullivan <jsullivan2847@gmail.com>
2022-07-10 12:35:37 -0400

    renders template at ''

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,7 @@
+from typing import List
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+def Home(request):
+    return render(request,'home.html')
+    
