9706a3e1f07dbe90efa0f894a2762e11c423d86b
James Sullivan <jsullivan2847@gmail.com>
2022-07-10 13:39:15 -0400

    User signup and login works

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,24 @@
+from multiprocessing import context
 from typing import List
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 def Home(request):
     return render(request,'home.html')
-    
+
+def signup(request):
+    error = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('')
+        else:
+            error = "Invalid sign up, try again"
+    form = UserCreationForm()
+    context = {'form': form, 'error': error}
+    return render(request, 'registration/signup.html', context)
+
