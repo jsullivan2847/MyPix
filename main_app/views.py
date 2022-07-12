cbfc2bf7ad18dc27ecb3e97cdca34cd9da523a60
James Sullivan <jsullivan2847@gmail.com>
2022-07-12 14:34:59 -0400

    Photo posting and viewing is complete

--- main_app/views.py
+++ main_app/views.py
@@ -51,13 +51,14 @@ def AddProfile(request):
 #to post
 def PostPhoto(request, profile_id):
         photo_file = request.FILES.get('photo_file', None)
+        profile = Profile.objects.get(id = profile_id)
         if photo_file:
             S3 = boto3.client('s3')
             key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
         try: 
             S3.upload_fileobj(photo_file, BUCKET, key)
             url = f"{S3_BASE_URL}{BUCKET}/{key}"
-            photo = Photo(photo_url = url, user = profile_id)
+            photo = Photo(photo_url = url, user = profile)
             photo.save()
             # profile = Profile.objects.get(id = profile_id)
             # profile.save()
